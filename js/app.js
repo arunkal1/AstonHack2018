1822156f20b9aa8f412fbe4a31371f5f7eba28f8
tech-a49 <tom.e.beckett@gmail.com>
2018-11-10 18:12:07 +0000

    added floors, problmes with falling off floors

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
   var player = $(".player");
   var board = $(".container");
+  var floor = $(".floor");
+  var floors = []
+  function getFloors(){
+    for(i of [1,2]){
+      floors.push($("#floor"+i))
+    }
+    console.log(floors)
+  }
+  getFloors();
 
   var keyPress = [];
 
@@ -20,6 +29,8 @@ $(document).ready(function() {
     boardPosition();
 
     characterPosition();
+
+    floorPosition();
 
     //horizontal movement
 
@@ -41,6 +52,17 @@ $(document).ready(function() {
     });
   }
 
+  function checkFloating(){
+
+    if ((playerBottom < floorsTop[1])) {
+      yacceleration = 0.1;
+      setCharPos();
+      move();
+      verticalCollisions();
+      floorCollision();
+    }
+  }
+
   var playerRight;
   var playerLeft;
   var playerTop;
@@ -50,7 +72,7 @@ $(document).ready(function() {
   var yacceleration;
   var yvelocity;
 
-  var ypos;
+  var ypos = 449;
 
   var jumping = false;
   var pressed = false;
@@ -62,6 +84,11 @@ $(document).ready(function() {
   var boardTop;
   var boardBott;
 
+  var floorsLeft = [];
+  var floorsRight = [];
+  var floorsTop = [];
+  var floorsBott = [];
+
   $("body").keydown(function(e) {
     //start the game with spacebar
     if (e.keyCode == 38 && pressed == false && jumping == false) {
@@ -69,11 +96,12 @@ $(document).ready(function() {
       jumping = true;
       yacceleration = 0.1;
       yvelocity = -5;
-      ypos = 449;
       pressed = true;
       console.log("jump");
     }
   });
+
+
   function characterPosition() {
     // Find the left and top edge of the player
     playerLeft = player.offset().left;
@@ -94,15 +122,27 @@ $(document).ready(function() {
     boardBott = boardTop + board.height();
   }
 
+  function floorPosition(){
+    for(floor of floors){
+      floorsLeft.push(floor.offset().left);
+      floorsTop.push( floor.offset().top);
+
+      floorsRight.push(floor.offset().left + floor.width());
+      floorsBott.push(floor.offset().top + floor.height());
+    }
+  }
+
   function jump() {
     jumpInt = setInterval(function() {
       characterPosition();
       boardPosition();
+      floorPosition();
 
       //move the player and check if it has hit the ground
       setCharPos();
       move();
       verticalCollisions();
+      floorCollision();
     }, 5);
   }
 
@@ -127,14 +167,40 @@ $(document).ready(function() {
         //reset player variables
         yvelocity = 0;
         yacceleration = 0;
-        ypos = 449;
+        ypos = boardBott-player.height()-1;
         setCharPos();
         pressed = false;
       }
+
+    }
+  }
+
+  function floorCollision(){
+    if (jumping == true) {
+      jumping = false
+    }
+    if((playerBottom >= floorsTop[0] && playerLeft <= floorsRight[0] && playerRight >= floorsLeft[0])||(playerBottom >= floorsTop[1] && playerLeft <= floorsRight[1] && playerRight >= floorsLeft[1])){
+      clearInterval(jumpInt);
+      console.log("floor")
+      yvelocity = 0;
+      yacceleration = 0;
+      if (playerLeft >= floorsRight[0]) {
+        ypos = floorsTop[1]-player.height()-1;
+      }else{
+
+        ypos = floorsTop[0]-player.height()-1;
+      }
+      setCharPos();
+      pressed = false;
     }
   }
 
   setInterval(function() {
     movePlayer();
+    console.log(yacceleration)
+    if (yacceleration == 0) {
+      checkFloating();
+
+    }
   }, 5);
 });
