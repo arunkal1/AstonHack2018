1e3f7ccb0aae2dc46e8671fd4708d51c41a37b38
Arun Kalwan <aruns1@hotmail.co.uk>
2018-11-10 23:12:10 +0000

    JS file created for Day and Night Mode

---
+++ js/nightmode.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+  function dayAndNight() {
+    var score = 49;
+
+    if (score <= 50) {
+      document
+        .getElementsByClassName("container")[0]
+        .classList.add("container");
+      var star = document.getElementById("star");
+      star.parentElement.removeChild(star);
+    } else if (score >= 50) {
+      document
+        .getElementsByClassName("container")[0]
+        .classList.add("container_override");
+      var cloud = document.getElementById("cloud1");
+      var cloud2 = document.getElementById("cloud2");
+      var cloud3 = document.getElementById("cloud3");
+      cloud.parentElement.removeChild(cloud);
+      cloud2.parentElement.removeChild(cloud2);
+      cloud3.parentElement.removeChild(cloud3);
+    }
+  }
+
+  dayAndNight();
+});
