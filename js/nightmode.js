1f7ce07b99fe45dc16745b881b531a5bc918c94b
Arun Kalwan <aruns1@hotmail.co.uk>
2018-11-11 02:13:22 +0000

    Changed boundaries for scores

--- js/nightmode.js
+++ js/nightmode.js
@@ -1,18 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
   var score = 0;
-  var coins = [];
-  // Temp variable to indicate player x coordinate
-  var playerXposition = 76;
 
   function dayAndNight() {
-    var coin1 = new coin(75, 100, 1);
-    coins.push(coin1);
-    var coin2 = new coin(100, 100, 2);
-    coins.push(coin2);
+    scoringSystem();
 
-    checkCoins();
-
-    if (score <= 50) {
+    if (score <= 20) {
       document
         .getElementsByClassName("container")[0]
         .classList.add("container");
@@ -22,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
       star2.parentElement.removeChild(star2);
       var star3 = document.getElementById("star3");
       star3.parentElement.removeChild(star3);
-    } else if (score >= 50) {
+    } else if (score >= 21) {
       document
         .getElementsByClassName("container")[0]
         .classList.add("container_override");
@@ -37,28 +29,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
   dayAndNight();
 
-  function coin(xPos, yPos, id) {
-    this.xPos = xPos;
-    this.yPos = yPos;
-    this.id = id;
-    $(".container").append(
-      "<img src='images/coin.png' class='coin' id='coin-" + id + "'/>"
-    );
-    $("#coin-" + id).css({
-      left: xPos,
-      top: yPos
-    });
-  }
-
-  // Run each time the player moves
-  function checkCoins() {
-    // Checks if player has a higher x coordinate than the coin. If true then remove the coin and increase points.
-    for (var i = 0; i < coins.length; i++) {
-      if (playerXposition >= coins[i].xPos) {
-        $("#coin-" + coins[i].id).remove();
-        score += 5;
-        $("#scoreBoard").text(score);
-      }
+  function scoringSystem() {
+    if (document.querySelector("#coin1") !== null) {
+      score += 100;
+      console.log(score);
     }
   }
 });
