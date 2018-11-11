d60e2f96e59883fd7f811adbd9bd1a807488cf8b
Arun Kalwan <aruns1@hotmail.co.uk>
2018-11-11 09:08:36 +0000

    Minor changes

--- js/app.js
+++ js/app.js
@@ -461,9 +461,9 @@ $(document).ready(function() {
     checkFloating();
   }, 5);
 
-  setInterval(function(){
+  setInterval(function() {
     dayAndNight();
-  },1000)
+  }, 1000);
 
   // floorPosition();
 
@@ -517,30 +517,27 @@ $(document).ready(function() {
   coins.push(coin2);
 
   function dayAndNight() {
-
-
     checkCoins();
     console.log(score);
-      if (score <= 4) {
-        document.getElementsByClassName("box")[0].classList.add("box");
-        var star = document.getElementById("star");
-        star.parentElement.removeChild(star);
-        var star2 = document.getElementById("star2");
-        star2.parentElement.removeChild(star2);
-        var star3 = document.getElementById("star3");
-        star3.parentElement.removeChild(star3);
-      } else if (score > 4) {
-        document
-          .getElementsByClassName("box")[0]
-          .classList.add("container_override");
-        var cloud = document.getElementById("cloud1");
-        var cloud2 = document.getElementById("cloud2");
-        var cloud3 = document.getElementById("cloud3");
-        cloud.parentElement.removeChild(cloud);
-        cloud2.parentElement.removeChild(cloud2);
-        cloud3.parentElement.removeChild(cloud3);
-      }
-    
+    if (score <= 4) {
+      document.getElementsByClassName("box")[0].classList.add("box");
+      var star = document.getElementById("star");
+      star.parentElement.removeChild(star);
+      var star2 = document.getElementById("star2");
+      star2.parentElement.removeChild(star2);
+      var star3 = document.getElementById("star3");
+      star3.parentElement.removeChild(star3);
+    } else if (score > 4) {
+      document
+        .getElementsByClassName("box")[0]
+        .classList.add("container_override");
+      var cloud = document.getElementById("cloud1");
+      var cloud2 = document.getElementById("cloud2");
+      var cloud3 = document.getElementById("cloud3");
+      cloud.parentElement.removeChild(cloud);
+      cloud2.parentElement.removeChild(cloud2);
+      cloud3.parentElement.removeChild(cloud3);
+    }
   }
 
   // dayAndNight();
